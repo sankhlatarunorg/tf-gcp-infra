7624311e4ee9f2bf0aef9c2d876aefcb69fd2a62
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-02-21 20:28:49 -0500

    change for vm creation

--- main.tf
+++ main.tf
@@ -34,3 +34,73 @@ resource "google_compute_route" "hoproute" {
   dest_range        = var.dest_range_route
   next_hop_gateway  = var.next_hop_gateway_route
 }
+
+// create vm from custom image gcp terraform
+resource "google_compute_instance" "webapp_vm" {
+  name         = "webapp-instance"
+  machine_type = "e2-standard-2"
+  zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image = "csye6225-image-a3"
+      type = "pd-balanced"
+    }
+  }
+
+  tags = ["allow-port-3000", "allow-port-5432"]
+  network_interface {
+    # network = "default"
+    network     = google_compute_network.csye6225_vpc_network.self_link
+    subnetwork  = google_compute_subnetwork.webapp.self_link
+    access_config {
+      
+    }
+  }
+}
+
+
+resource "google_compute_firewall" "allow_port_3000" {
+  name    = "allow-port-3000"
+  network = google_compute_network.csye6225_vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-port-3000"]
+}
+
+resource "google_compute_firewall" "allow_port_5432" {
+  name    = "allow-port-5432"
+  network = google_compute_network.csye6225_vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5432"]
+  }
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-port-5432"]
+}
+
+
+
+# resource "google_compute_firewall" "allow_internet_traffic" {
+#   name    = "allow-internet-traffic"
+#   network = google_compute_network.csye6225_vpc_network.self_link
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["3000"]
+#   }
+
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags   = ["allow-internet-traffic"]
+# }
