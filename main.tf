f52f364805469c0cc14d56dee641dc499f52e708
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-02-27 22:13:16 -0500

    changes for metdata

--- main.tf
+++ main.tf
@@ -7,7 +7,8 @@ provider "google" {
 
 
 resource "google_compute_network" "csye6225_vpc_network" {
-  name                            = var.network
+  count                           = length(var.vpc_network_list)
+  name                            = var.vpc_network_list[count.index]
   auto_create_subnetworks         = var.vpc_auto_create_subnetworks
   routing_mode                    = var.vpc_routing_mode
   delete_default_routes_on_create = var.vpc_delete_default_routes_on_create
@@ -18,24 +19,23 @@ resource "google_compute_subnetwork" "webapp" {
   name          = var.subnetwork_webapp
   ip_cidr_range = var.webapp_subnetwork_ip_cidr_range
   region        = var.vpc_region
-  network       = google_compute_network.csye6225_vpc_network.id
+  network       = google_compute_network.csye6225_vpc_network[0].id
 }
 
 resource "google_compute_subnetwork" "db" {
   name          = var.subnetwork_db
   ip_cidr_range = var.db_subnetwork_ip_cidr_range
   region        = var.vpc_region
-  network       = google_compute_network.csye6225_vpc_network.id
+  network       =  google_compute_network.csye6225_vpc_network[0].id
 }
 
 resource "google_compute_route" "hoproute" {
-  name              = var.route_hop
-  network           = google_compute_network.csye6225_vpc_network.self_link
-  dest_range        = var.dest_range_route
-  next_hop_gateway  = var.next_hop_gateway_route
+  name             = var.route_hop
+  network          = google_compute_network.csye6225_vpc_network[0].self_link
+  dest_range       = var.dest_range_route
+  next_hop_gateway = var.next_hop_gateway_route
 }
 
-// create vm from custom image gcp terraform
 resource "google_compute_instance" "webapp_vm" {
   name         = var.webapp_vm_name
   machine_type = var.machine_type
@@ -43,47 +43,54 @@ resource "google_compute_instance" "webapp_vm" {
   boot_disk {
     initialize_params {
       image = var.base_image_name
-      type = var.base_image_type
-      size = var.boot_disk_size
+      type  = var.base_image_type
+      size  = var.boot_disk_size
     }
   }
-
-  tags = [var.allow_port_3000_name, var.allow_port_5432_name,var.deny_port_22_name,var.allow_postgres]
+  allow_stopping_for_update = true
+  tags                      = var.firewall_policy_to_apply_name
   network_interface {
-    # network = "default"
-    network     = google_compute_network.csye6225_vpc_network.self_link
-    subnetwork  = google_compute_subnetwork.webapp.self_link
+    network    = google_compute_network.csye6225_vpc_network[0].self_link
+    subnetwork = google_compute_subnetwork.webapp.self_link
     access_config {
-      
+
     }
   }
-  metadata = google_compute_project_metadata.web_metadata.metadata
+  service_account {
+    email  = var.service_account_email
+    scopes = var.service_account_scopes
+  }
+    metadata = google_compute_project_metadata.web_metadata.metadata
 
-  metadata_startup_script = <<-EOF
+    
+    metadata_startup_script = <<-EOF
 #!/bin/bash
-
-MARKER_FILE="/var/run/webapp_configured"
-ENV_FILE="/tmp/webapp/.env"
+echo "Web application is starting up..."
 if [[ ! -e /tmp/webapp/.env ]]; then
-    touch /tmp/webapp/.env
+  touch /tmp/webapp/.env
 fi
 
-if [ -f "$MARKER_FILE" ]; then
-  echo "Web application is already configured. Skipping configuration."
-else
-
-  echo "DB_USER=\$(curl -H \"Metadata-Flavor: Google\" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_USER)" >> $ENV_FILE
-  echo "DB_PASSWORD=\$(curl -H \"Metadata-Flavor: Google\" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_PASSWORD)" >> $ENV_FILE
-  echo "DB_HOST=\$(curl -H \"Metadata-Flavor: Google\" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_HOST)" >> $ENV_FILE
-  echo "DB_NAME=\$(curl -H \"Metadata-Flavor: Google\" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_NAME)" >> $ENV_FILE
+if [[  ! -e "/var/run/webapp_configured" ]]; then
+  touch $Mark_FILE
+  echo "DB_USER=\$(curl -H \"Metadata-Flavor: Google\" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_USER)" >> /tmp/webapp/.env
+  echo "DB_PASSWORD=\$(curl -H \"Metadata-Flavor: Google\" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_PASSWORD)" >> /tmp/webapp/.env
+  echo "DB_HOST=\$(curl -H \"Metadata-Flavor: Google\" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_HOST)" >> /tmp/webapp/.env
+  echo "DB_NAME=\$(curl -H \"Metadata-Flavor: Google\" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_NAME)" >> /tmp/webapp/.env
   # Additional configurations...
 
-  # Create the marker file to indicate successful configuration
-  sudo touch $MARKER_FILE
+  touch /var/run/webapp_configured
+else
+  echo "Web application is already configured."
 fi
 sudo systemctl stop csye-6225
 sudo systemctl start csye-6225
 sudo systemctl status csye-6225
+echo "Web application is configured."
+echo $DB_USER
+echo $DB_PASSWORD
+echo $DB_HOST
+echo $DB_NAME
+echo "Web application is running."
 EOF
 }
 
@@ -92,64 +99,53 @@ resource "google_compute_project_metadata" "web_metadata" {
     "DB_NAME"       = var.webapp_DB_Name
     "DB_USER"       = var.webapp_DB_Name
     "DB_PASSWORD"   = random_password.password.result
-    "DB_HOST"       = var.global_address_address
+    "DB_HOST"       = google_sql_database_instance.webapp_sql_instance.private_ip_address
   }
 }
 
-resource "google_compute_firewall" "allow_port_3000" {
-  name    = var.allow_port_3000_name
-  network = google_compute_network.csye6225_vpc_network.self_link
+resource "google_compute_firewall" "custom_firewall_rules" {
+  count = length(var.firewall_rules_policy)
 
-  allow {
-    protocol = var.allow_tcp_port_protocol
-    ports    = [var.allow_port_3000]
+
+  name    = var.firewall_rules_policy[count.index].name
+  network = google_compute_network.csye6225_vpc_network[0].self_link
+
+  dynamic "allow" {
+    for_each = var.firewall_rules_policy[count.index].rule_type == var.allow ? [1] : []
+
+    content {
+      protocol = var.firewall_rules_policy[count.index].port_protocol
+      ports    = [var.firewall_rules_policy[count.index].port]
+    }
   }
 
-  source_ranges = [var.sourse_range_firewall]
-  target_tags   = [var.allow_port_3000_name]
-}
+  dynamic "deny" {
+    for_each = var.firewall_rules_policy[count.index].rule_type == var.deny ? [1] : []
 
-resource "google_compute_firewall" "allow_port_5432" {
-  name    = var.allow_port_5432_name
-  network = google_compute_network.csye6225_vpc_network.self_link
-
-  allow {
-    protocol = var.allow_tcp_port_protocol
-    ports    = [var.allow_port_5432]
+    content {
+      protocol = var.firewall_rules_policy[count.index].port_protocol
+      ports    = [var.firewall_rules_policy[count.index].port]
+    }
   }
 
-  source_ranges = [var.sourse_range_firewall]
-  target_tags   = [var.allow_port_5432_name]
+  source_ranges = [var.firewall_rules_policy[count.index].source_range]
 }
-
-resource "google_compute_firewall" "deny_port_22" {
-  name    = var.deny_port_22_name
-  network = google_compute_network.csye6225_vpc_network.self_link
-  deny {
-    protocol  = var.allow_tcp_port_protocol
-    ports     = [var.deny_port_22]
-  }
-  source_ranges   = [var.sourse_range_firewall]
-  target_tags     = [var.deny_port_22_name] 
-}
-
 
 resource "google_sql_database_instance" "webapp_sql_instance" {
   name             = "webapp-sql-instance-${random_id.random_db_instance_id.hex}"
   database_version = "MYSQL_8_0"
   region           = var.region
-
   depends_on = [ google_service_networking_connection.webapp_private_vpc_connection ]
   settings {
-    tier = "db-f1-micro"
-    edition = "ENTERPRISE"
+    tier                = "db-f1-micro"
+    edition             = "ENTERPRISE"
     disk_autoresize     = false
     disk_size           = 10
     disk_type           = "PD_HDD"
     availability_type   = "REGIONAL"
     ip_configuration {
       ipv4_enabled    = false
-      private_network = google_compute_network.csye6225_vpc_network.self_link
+      private_network = google_compute_network.csye6225_vpc_network[0].self_link
     }
     backup_configuration {
       binary_log_enabled = true
@@ -164,14 +160,14 @@ resource "google_compute_global_address" "global_address" {
   project       = var.project
   name          = var.global_address_name
   purpose       = var.global_address_purpose
-  network       = google_compute_network.csye6225_vpc_network.id
-  address_type = "INTERNAL"
-  address       = var.global_address_address
+  network       = google_compute_network.csye6225_vpc_network[0].self_link
+  address_type  = "INTERNAL"
+  prefix_length = 16
 }
 
 resource "google_service_networking_connection" "webapp_private_vpc_connection" {
-  provider              = google-beta
-  network                 = google_compute_network.csye6225_vpc_network.id
+  provider                = google-beta
+  network                 = google_compute_network.csye6225_vpc_network[0].self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.global_address.name]
 }
@@ -182,9 +178,9 @@ resource "google_sql_database" "sql_database" {
 }
 
 resource "google_sql_user" "sql_user" {
-  name = var.webapp_DB_Name
-  instance = google_sql_database_instance.webapp_sql_instance.name
-  password = random_password.password.result
+  name      = var.webapp_DB_Name
+  instance  = google_sql_database_instance.webapp_sql_instance.name
+  password  = random_password.password.result
 }
 
 resource "random_password" "password" {
@@ -197,3 +193,26 @@ resource "random_password" "password" {
 resource "random_id" "random_db_instance_id" {
   byte_length = 8
 }
+
+
+# resource "google_compute_firewall" "allow_port_3000" {
+#   name    = var.allow_port_3000_name
+#   network = google_compute_network.csye6225_vpc_network.self_link
+
+#   allow {
+#     protocol = var.allow_tcp_port_protocol
+#     ports    = [var.allow_port_3000]
+#   }
+
+#   source_ranges = [var.sourse_range_firewall]
+# }
+
+# resource "google_compute_firewall" "deny_port_22" {
+#   name    = var.deny_port_22_name
+#   network = google_compute_network.csye6225_vpc_network.self_link
+#   allow {
+#     protocol  = var.allow_tcp_port_protocol
+#     ports     = [var.deny_port_22]
+#   }
+#   source_ranges   = [var.sourse_range_firewall]
+# }
